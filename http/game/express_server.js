ccfd9990efe1f402059a2118b8dff89918ddf870
何晏波 <heyanbo@medlinker.com>
2021-06-11 16:33:51 +0800

    chore: add express

--- http/game/express_server.js
+++ http/game/express_server.js
@@ -7,28 +7,23 @@ const game = require("./game");
 const app = express();
 
 app.get("/favicon.ico", function (req, res) {
-  res.writeHead(200);
-  res.end();
+  res.status(200);
 });
 
 app.get("/", function (req, res) {
-  fs.createReadStream(__dirname + "/index.html").pipe(res);
+  res.send(fs.readFileSync(__dirname + "/index.html", "utf-8"));
 });
 
 app.get("/game", function (req, res) {
   const Url = url.parse(req.url);
   const playAction = qs.parse(Url.query).playAction;
   if (game(playAction) === 1) {
-    res.writeHead(200, { "content-type": "text/html;charset=utf-8" });
-    res.end("你赢了", "utf8");
+    res.send("你赢了");
   } else if (game(playAction) === -1) {
-    res.writeHead(200, { "content-type": "text/html;charset=utf-8" });
-    res.end("你输了", "utf8");
+    res.send("你输了");
   } else {
-    res.writeHead(200, { "content-type": "text/html;charset=utf-8" });
-    res.end("平局!", "utf8");
+    res.send("平局");
   }
-  fs.createReadStream(__dirname + "/index.html").pipe(res);
 });
 
 app.listen(3000);
